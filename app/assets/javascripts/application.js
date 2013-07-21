3683438c4119d1a1887fefc2716e86bf25272e90
blibotics <blibotics@gmail.com>
2013-07-20 21:26:38 -0400

    Removing all javascript, rebuilding clicking edit functionality from scratch to incorporate ajax

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -16,40 +16,5 @@
 //= require_tree .
 
 $(document).ready(function(){
-  $(".tudu-edit").on("click", function(){
-    var p = $(this).parent();
-    var tuTask = p.find(".tudu-task");
 
-    if(!$(this).hasClass("clicked")) {
-      $(".tudu-list-task").find(".tudu-edit").removeClass("clicked");
-      $(".tudu-list-task").find(".tudu-edit-input").remove();
-      $(".tudu-list-task").find(".tudu-edit").text("Edit");
-      $(".tudu-list-task").find(".tudu-task").show();
-      $(".tudu-list-task").find(".update-cancel").remove();
-      $(this).addClass("clicked");
-      tuTask.hide();
-      tuTask.after('<input value="'+tuTask.text()+'" class="tudu-edit-input"></input>');
-      $(this).text("Update ");
-      $(this).after('<span class="update-cancel"> | <span class="update-cancel-click">Cancel</span></span>');
-    }
-    else {
-      $(this).removeClass("clicked");
-      tuTask.show();
-      p.find(".tudu-edit-input").remove();
-      $(this).text("Edit");
-      $(".tudu-list-task").find(".update-cancel").remove();
-    }
-
-    $(".update-cancel-click").on("click", function(){
-      $(this).removeClass("clicked");
-      tuTask.show();
-      p.find(".tudu-edit-input").remove();
-      $(this).text("Edit");
-      $(".tudu-list-task").find(".update-cancel").remove();
-
-    });
-
-
-
-  });
 });
