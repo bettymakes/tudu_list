78ea5e3f506a9e50a41fab95a67fb801198ff1f9
blibotics <blibotics@gmail.com>
2013-07-20 23:09:14 -0400

    Updating styling, removing redirection for update action

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -27,11 +27,11 @@ class ListsController < ApplicationController
 
   def update
     @list = List.find(params[:id])
-    if @list.update_attributes(list_params)
-      redirect_to lists_path
-    else
-      render :new
-    end
+    @list.update_attributes(list_params)
+    #   redirect_to lists_path
+    # else
+    #   render :new
+    # end
   end
 
   def destroy
