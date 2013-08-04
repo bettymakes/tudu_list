ace45af8b297fae2f47f37fea764ca8800f49dc4
blibotics <blibotics@gmail.com>
2013-08-04 00:34:55 -0400

    Renaming 'task' attributes to 'item' in the controller

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -51,7 +51,7 @@ class TasksController < ApplicationController
   private
     def task_params
       params.require(:task).permit(
-        !!:task,!!
+        :item,
         :priority,
         :order_num
       )
