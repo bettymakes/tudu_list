30fb64729bcc2e599d856a50a49b9ce73c5f5a5b
blibotics <blibotics@gmail.com>
2013-08-04 01:13:46 -0400

    Revert "Renaming all 'lists' related variables to 'tasks' in all other controllers"
    This reverts commit e38bc6aa4dacc574b0a61fa1b705d0ba697964bc.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,7 +7,7 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
     if @user.save
       auto_login(@user)
-      redirect_to tasks_path
+      redirect_to lists_path
     else
       render :new
     end
