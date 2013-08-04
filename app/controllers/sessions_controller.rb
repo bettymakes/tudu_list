2709de07ee3e1ecac88e767e152eb0e0be76b9f2
blibotics <blibotics@gmail.com>
2013-08-03 22:49:00 -0400

    Adding logic to create action and Creating login view

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,6 +3,13 @@ class SessionsController < ApplicationController
   end
 
   def create
+    user = login(params[:email], params[:password])
+    if user
+      redirect_to lists_path
+    else
+      flash.now.alert = "Email or password was invalid. Please try again."
+      render :new
+    end
   end
 
   def destroy
